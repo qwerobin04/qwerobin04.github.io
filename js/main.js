77560554a77965e852c043610f81416a69b36c08
robinabanes <abanesjohnrobin@gmail.com>
2020-02-28 01:39:20 +0800

    version 2.0

--- js/main.js
+++ js/main.js
@@ -1,117 +1,268 @@
-(function ($) {
-	"use strict";
-	var nav = $('nav');
-  var navHeight = nav.outerHeight();
+/* -----------------------------------------------
+					Js Main
+--------------------------------------------------
 
-  $('.navbar-toggler').on('click', function() {
-    if( ! $('#mainNav').hasClass('navbar-reduce')) {
-      $('#mainNav').addClass('navbar-reduce');
-    }
-  })
+    Template Name: Baha - Personal Portfolio Template
+    Author: Malyarchuk
+    Copyright: 2019
 
-  // Preloader
-  $(window).on('load', function () {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
-        $(this).remove();
-      });
-    }
-  });
+--------------------------------------------------
 
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-  $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
-    return false;
-  });
+Table of Content
 
-	/*--/ Star ScrollTop /--*/
-	$('.scrolltop-mf').on("click", function () {
-		$('html, body').animate({
-			scrollTop: 0
-		}, 1000);
-	});
+	1. Preloader
+	2. Sound Start
+	3. Isotope Portfolio Setup
+	4. Blogs Masonry Setup
+	5. Active Current Link
+	6. Mobile Toggle Click Setup
+	7. Testimonials OwlCarousel
+	8. Chart Setup
+	9. Portfolio Tilt Setup
+	10. Portfolio Image Link
+	11. Portfolio Video Link
+	12. Blog Video Link
+	13. Validate Contact Form
+	14. Google Map
 
-	/*--/ Star Counter /--*/
-	$('.counter').counterUp({
-		delay: 15,
-		time: 2000
-	});
+----------------------------------- */
 
-	/*--/ Star Scrolling nav /--*/
-	$('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: (target.offset().top - navHeight + 2)
-				}, 1000, "easeInOutExpo");
-				return false;
+$(window).on('load', function() {
+		
+	/* -----------------------------------
+				1. Preloader
+	----------------------------------- */
+	$("#preloader").delay(1000).addClass('loaded');
+	
+	/* -----------------------------------
+			  2. Sound Setup
+	----------------------------------- */
+	$('body').append('<audio loop autoplay volume="1" id="audio-player"><source src="music.mp3" type="audio/mpeg"></audio>');
+    	var audio = document.getElementById("audio-player");
+    	audio.volume = 0.2;
+	
+	if($(window).length) {
+		$('.music-bg').css({'visibility':'visible'});
+		$('body').addClass("audio-on");
+		if ($('body').hasClass('audio-off')) {
+        	$('body').removeClass('audio-on');
+		} 
+		$(".music-bg").on('click', function() {
+			$('body').toggleClass("audio-on audio-off");         
+			if ($('body').hasClass('audio-off')) {
+				audio.pause();
+			} 
+			if ($('body').hasClass('audio-on')) {
+				audio.play();
 			}
-		}
-	});
-
-	// Closes responsive menu when a scroll trigger link is clicked
-	$('.js-scroll').on("click", function () {
-		$('.navbar-collapse').collapse('hide');
-	});
-
-	// Activate scrollspy to add active class to navbar items on scroll
-	$('body').scrollspy({
-		target: '#mainNav',
-		offset: navHeight
-	});
-	/*--/ End Scrolling nav /--*/
-
-	/*--/ Navbar Menu Reduce /--*/
-	$(window).trigger('scroll');
-	$(window).on('scroll', function () {
-		var pixels = 50;
-		var top = 1200;
-		if ($(window).scrollTop() > pixels) {
-			$('.navbar-expand-md').addClass('navbar-reduce');
-			$('.navbar-expand-md').removeClass('navbar-trans');
-		} else {
-			$('.navbar-expand-md').addClass('navbar-trans');
-			$('.navbar-expand-md').removeClass('navbar-reduce');
-		}
-		if ($(window).scrollTop() > top) {
-			$('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
-		} else {
-			$('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
-		}
-	});
-
-	/*--/ Star Typed /--*/
-	if ($('.text-slider').length == 1) {
-    var typed_strings = $('.text-slider-items').text();
-		var typed = new Typed('.text-slider', {
-			strings: typed_strings.split(','),
-			typeSpeed: 80,
-			loop: true,
-			backDelay: 1100,
-			backSpeed: 30
 		});
 	}
+	
+	/* -----------------------------------
+			3. Isotope Portfolio Setup
+	----------------------------------- */
+    if( $('.portfolio-items').length ) {
+        var $elements = $(".portfolio-items"),
+            $filters = $('.portfolio-filter ul li');
+        $elements.isotope();
 
-	/*--/ Testimonials owl /--*/
-	$('#testimonial-mf').owlCarousel({
-		margin: 20,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 1,
-			}
-		}
+        $filters.on('click', function(){
+            $filters.removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).data('filter');
+            $(".portfolio-items").isotope({
+                filter: selector,
+                hiddenStyle: {
+                    transform: 'scale(.2) skew(30deg)',
+                    opacity: 0
+                },
+                visibleStyle: {
+                    transform: 'scale(1) skew(0deg)',
+                    opacity: 1,
+                },
+                transitionDuration: '.5s'
+            });
+        });
+    }
+	
+	/* -----------------------------------
+			4. Blogs Masonry Setup
+	----------------------------------- */
+    $('.blog-masonry').isotope({ layoutMode: 'moduloColumns' });
+	
+});
+
+$(document).ready(function() {
+    "use strict";
+	
+	/* -----------------------------------
+			5. Active Current Link
+	----------------------------------- */
+    $('.header-main ul li a').on('click',function() {
+        if($('.header-main.on').length) {
+            $('.header-main').removeClass('on');
+        }
+    });
+	
+	/* -----------------------------------
+		6. Mobile Toggle Click Setup
+	----------------------------------- */
+    $('.header-toggle').on('click', function() {
+        $('.header-main').toggleClass('on');
+    });
+	
+	/* -----------------------------------
+	      7. Testimonials OwlCarousel
+	----------------------------------- */
+	$(".testimonial .owl-carousel").owlCarousel({
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        smartSpeed: 500,
+        responsiveClass: true,
+        dots: false,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            800: {
+                items: 1,
+            },
+            1000: {
+                items: 2,
+            },
+        },
+    });
+	
+	/* -----------------------------------
+	      	8. Chart Setup
+	----------------------------------- */
+	if ($('.chart').length > 0) {
+	    $('.chart').easyPieChart({
+          trackColor:'#0e0f10',
+	      scaleColor:false,
+	      easing: 'easeOutBounce',
+	      scaleLength: 4,
+	      lineCap: 'square',
+	      lineWidth:5,
+	      size:130,
+	      animate: {
+	                duration: 2500,
+	                enabled: true
+	    	}
+	 	});
+	 }
+	
+	/* -----------------------------------
+	      	9. Portfolio Tilt Setup
+	----------------------------------- */
+    $('.pt-portfolio .portfolio-items .item figure').tilt({
+        maxTilt: 3,
+        glare: true,
+        maxGlare: .6,
+        reverse: true
+    });
+	
+	/* -----------------------------------
+	      10. Portfolio Image Link
+	----------------------------------- */
+	$(".portfolio-items .image-link").magnificPopup({
+		type: "image"
 	});
+	
+	/* -----------------------------------
+	      11. Portfolio Video Link
+	----------------------------------- */
+	$(".portfolio-items .video-link").magnificPopup({
+		type: "iframe"
+	});
+	
+	/* -----------------------------------
+	      12. Blog Video Link
+	----------------------------------- */
+	$(".pt-blog .blog-item .thumbnail .btn-play").magnificPopup({
+		type: "iframe"
+	});
+	
+	/* -----------------------------------
+	    13. Validate Contact Form
+	----------------------------------- */
+	if ($("#contact-form").length) {
+        $("#contact-form").validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
 
-})(jQuery);
+                email: "required",
+				
+            },
+
+            messages: {
+                name: "Please enter your name",
+                email: "Please enter your email address"
+            },
+
+            submitHandler: function (form) {
+                $.ajax({
+                    type: "POST",
+                    url: "mail.php",
+                    data: $(form).serialize(),
+                    success: function () {
+                        $( "#loader").hide();
+                        $( "#success").slideDown( "slow" );
+                        setTimeout(function() {
+                        $( "#success").slideUp( "slow" );
+                        }, 3000);
+                        form.reset();
+                    },
+                    error: function() {
+                        $( "#loader").hide();
+                        $( "#error").slideDown( "slow" );
+                        setTimeout(function() {
+                        $( "#error").slideUp( "slow" );
+                        }, 3000);
+                    }
+                });
+                return false;
+            }
+
+        });
+    }
+	
+	/* Google Map Setup */
+    if($('#map').length) {
+        initMap();
+     };
+
+});
+
+/* -----------------------------------
+  		14. Google Map
+----------------------------------- */
+function initMap() {
+    var latitude = $("#map").data('latitude'),
+        longitude = $("#map").data('longitude'),
+        zoom = $("#map").data('zoom'),
+        cordinates = new google.maps.LatLng(latitude, longitude);
+
+    var styles = [{"stylers":[{"saturation":-100},{"gamma":0.8},{"lightness":4},{"visibility":"on"}]},{"featureType":"landscape.natural","stylers":[{"visibility":"on"},{"color":"#5dff00"},{"gamma":4.97},{"lightness":-5},{"saturation":100}]}];
+	
+        var mapOptions = {
+        zoom: zoom,
+        center: cordinates,
+        mapTypeControl: false,
+        disableDefaultUI: true,
+        zoomControl: true,
+        scrollwheel: false,
+        styles: styles
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var marker = new google.maps.Marker({
+        position: cordinates,
+        map: map,
+        title: "We are here!"
+    });
+}
